a1ce973696da258d5ba74660e254d1baa04b9386
avkoehl <avkoehl@ucdavis.edu>
2018-01-17 15:16:52 -0800

    Frequency per timeshot working

--- readCorpus.py
+++ readCorpus.py
@@ -1,6 +1,7 @@
 # PYTHON 2
 import os
-import nltk
+from nltk import word_tokenize
+import math
 
 
 ##
@@ -12,6 +13,11 @@ filelist = sorted(filelist)
 
 # page breaks weeks: 0 - 9
 pages = ["00250023", "00970095", "01830181", "02120210", "02650263", "03530351", "04850483", "05870585", "07210719", "00930091"]
+dates = ["Dec 03 - Dec 09", "Dec 10 - Dec 16", "Dec 17 - Dec 23", 
+        "Dec 24 - Dec 30", "Dec 31 - Jan 06", "Jan 07 - Jan 13", 
+        "Jan 14 - Jan 20", "Jan 21 - Jan 27", "Jan 28 - Feb 03", 
+        "Feb 10 - Feb 17"]
+
 files = []
 
 
@@ -19,6 +25,7 @@ pb = 0
 fnames = []
 texts = []
 text = []
+
 for i in range(0, len(filelist)):
 
     full = filelist[i]
@@ -39,4 +46,28 @@ for i in range(0, len(filelist)):
 
 # Now in texts[0] is all the text up to 00250023 and texts[1] has 00250023 to 00970095 and so on
 # need to tokenize each element in texts and then calculate word frequencies, will use nltk
+searchword = "union"
+freqs = []
+occs = []
+totals = []
+for i in range (0, len(texts)):
+    raw = texts[i].decode('utf8').lower()
+    words = word_tokenize(raw)
+    occurances = words.count(searchword)
+    total = len(words)
+    frequency = float(occurances) / float(total)
+
+    occs.append(occurances)
+    totals.append(total)
+
+    if (frequency > 0):
+        freqs.append(math.log10(frequency))
+    else:
+        freqs.append(0)
+
+print "searchword:", searchword
+for i in range(0, len(freqs)):
+    print dates[i], "|", freqs[i]
+    
+
 
