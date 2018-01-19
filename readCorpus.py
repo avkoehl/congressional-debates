ab8de55a175b266926e2ab82fbe0fedd580381f9
avkoehl <avkoehl@ucdavis.edu>
2018-01-19 11:55:01 -0800

    now readCorpus outputs a pickle object that is the string of sorted texts, to be read in by the three different models (frequency ...)

--- readCorpus.py
+++ readCorpus.py
@@ -2,6 +2,7 @@
 import os
 from nltk import word_tokenize
 import math
+import pickle
 
 
 ##
@@ -12,7 +13,12 @@ filelist = os.listdir(path)
 filelist = sorted(filelist)
 
 # page breaks weeks: 0 - 9
-pages = ["00250023", "00970095", "01830181", "02120210", "02650263", "03530351", "04850483", "05870585", "07210719", "09930991"]
+pages = ["00250023", "00970095", "01830181",
+        "02120210", "02650263", "03530351", 
+        "04850483", "05870585", "07210719", 
+        "09930991"]
+
+# Coresponding Dates for weeks 0 - 9
 dates = ["Dec 03 - Dec 09", "Dec 10 - Dec 16", "Dec 17 - Dec 23", 
         "Dec 24 - Dec 30", "Dec 31 - Jan 06", "Jan 07 - Jan 13", 
         "Jan 14 - Jan 20", "Jan 21 - Jan 27", "Jan 28 - Feb 03", 
@@ -44,33 +50,7 @@ for i in range(0, len(filelist)):
         fnames = []
         text = []
 
-# Now in texts[0] is all the text up to 00250023 and texts[1] has 00250023 to 00970095 and so on
-# need to tokenize each element in texts and then calculate word frequencies, will use nltk
-searchword = "union"
-freqs = []
-occs = []
-totals = []
-for i in range (0, len(texts)):
-    raw = texts[i].decode('utf8').lower()
-    words = word_tokenize(raw)
-    occurances = words.count(searchword)
-    total = len(words)
-    frequency = float(occurances) / float(total)
 
-    occs.append(occurances)
-    totals.append(total)
-
-    if (frequency > 0):
-        freqs.append(math.log10(frequency))
-    else:
-        freqs.append(0)
-
-print "searchword:", searchword
-for i in range(0, len(freqs)):
-    print dates[i], "|", freqs[i]
-    
-
-import matplotlib.pyplot as plt
-plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], freqs, 'ro')
-plt.show()
+with open('texts.pkl', 'wb') as f:
+   pickle.dump(text, f)
 
