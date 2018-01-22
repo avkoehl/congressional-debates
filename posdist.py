806593c446f7141cb45df6b7dc46240b369f218a
avkoehl <avkoehl@ucdavis.edu>
2018-01-22 14:40:25 -0800

    posdist, no visualization done, create ts using jsd

--- posdist.py
+++ posdist.py
@@ -1,8 +1,15 @@
 #python2
+from nltk import sent_tokenize
 from nltk import word_tokenize
+from nltk import pos_tag
 import math
 import pickle
 import matplotlib.pyplot as plt
+
+# Please make it stop
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 
@@ -27,10 +34,50 @@ dates = ["Dec 03 - Dec 09", "Dec 10 - Dec 16", "Dec 17 - Dec 23",
 
 # Now in texts[0] is all the text up to 00250023 and texts[1] has 00250023 to 00970095 and so on
 # need to tokenize each element in texts and then calculate word frequencies, will use nltk
-searchword = "union"
-for i in range (0, len(texts)):
-    raw = texts[i].decode('utf8').lower()
+searchword = "work" #work is a good word to see multiple parts of speech
+all_sentences = []
+sentences = []
+parts_of_speech = [] #for all the texts, should have length 9 (one for each week)
 
-plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dist, 'ro')
-plt.show()
+for i in range (0, len(texts)):
+    parts = [] #for each text, to be appended into parts_of_speech
+    raw = texts[i].decode('utf8').lower()
+    all_sentences = sent_tokenize(raw)
+    for sent in all_sentences:
+        if searchword in sent:
+            sentences.append(sent)
+
+    for s in sentences:
+        pos = pos_tag(word_tokenize(s))
+        for j in range (0, len(pos)):
+            if str(pos[j][0]) == searchword:
+                parts.append(pos[j][1])
+    parts_of_speech.append(parts)
+
+
+## given all the parts of speech for the word in each text (week), calculate distributions
+## Calculate the Jensen Shannon Divergence for each text (week)
+for i in range(0, len(parts_of_speech)):
+    print "=================================================================="
+    print "Week Number:", i
+    print "=================================================================="
+    unique = set(parts_of_speech[i])
+    total = len(parts_of_speech[i])
+    for pos in unique:
+        count = parts_of_speech[i].count(pos)
+        dist = float(count) / float(total)
+        print pos, count, 100 * dist
+
+   # formula for Jensen Shannon Divergence
+   # JSD is list with 10 values, one for each week
+
+
+
+## Plot the Jensen Shannon Divergence Time Series for each snapshot
+#plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], JSD, 'ro')
+#plt.show()
+
+
+## Plot the Stacked Area time series for part of speech distribution
+# not sure, look up
 
