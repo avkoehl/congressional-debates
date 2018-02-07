3aa4b646ac6f8e92bb3231d831ea078067daa579
avkoehl <avkoehl@ucdavis.edu>
2018-02-06 16:27:31 -0800

    frequency.py made simpler, cut lines that weren't necessary

--- TimeSeries/frequency.py
+++ TimeSeries/frequency.py
@@ -1,7 +1,5 @@
 #python 3
-import math
-import os
-import re
+import math, os, re
 import matplotlib.pyplot as plt
 
 ##
@@ -13,26 +11,19 @@ filelist = sorted(os.listdir(path))
 weeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 searchword = "state"
 freqs = []
-occs = []
-totals = []
 
 
 for i in range (0, len(weeks)):
     with open(path + str(i) + ".txt", "r") as myfile:
         doc = myfile.read()
         words = doc.split(" ")
-        occurances = words.count(searchword)
-        total = len(words)
-        frequency = float(occurances) / float(total)
-
-        occs.append(occurances)
-        totals.append(total)
+        frequency = float(words.count(searchword)) / len(words)
 
         if (frequency > 0):
             freqs.append(math.log10(frequency))
         else:
             freqs.append(0)
 
-plt.plot([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], freqs, 'ro')
+plt.plot(weeks, freqs, 'ro')
 plt.show()
 
