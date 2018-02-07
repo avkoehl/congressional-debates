8323a85aba68b8dd396801b11c758a6960c683b2
avkoehl <avkoehl@ucdavis.edu>
2018-02-07 14:33:02 -0800

    same as last commit

--- TimeSeries/distributional/distributional.py
+++ TimeSeries/distributional/distributional.py
@@ -1,15 +1,22 @@
 #python 3
 import os, time, gensim
+from gensim.models import translation_matrix
+from gensim.models import KeyedVectors
 
 """ Distributional model for Linguistic shift time sereis construction
         For each time slot,
             [1] load in gensime word2vec modles generated wiht gensim_models.py 
             [2] align all vectors to the last vector
+            [3] compute the distances between each vector and the first vector (just model["searchword"] vectors)
+            *** NOT SURE ABOUT THREE, AM I LOOKING AT ALIGNING ENTIRE VECTOR OR VECTORS FOR THE WORDS ...
+                I THINK JUST THE VECTORS FOR THE WORDS
             [3] plot a time series of the distances between vectors
 """
 
 ##
-path = '../Corpus/weeks/'
-filelist = sorted(os.listdir(path))
 weeks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(0, len(weeks)):
+    model = gensim.models.Word2Vec.load("./models/" + str(i) + ".model")
+
 
